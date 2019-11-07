34ce3b6780226be72ab446a159c5453b830c97f2
= <diegomarcel27@hotmail.com>
2019-11-07 18:47:14 -0300

    Se agregan mas pruebas para el worker de promociones creadas

--- test/workers/created_promotions_worker_test.rb
+++ test/workers/created_promotions_worker_test.rb
@@ -16,4 +16,41 @@ class CreatedPromotionsWorkerTest < ActiveSupport::TestCase
     end
   end
 
+  test 'should generate count registers for each age range' do
+    payload = {
+      promotion_id: 10,
+      organization_id: 3
+    }
+    # three age ranges defined in fixture
+    assert_difference('CountByAgeRange.count', 3) do
+      @worker.work(payload.to_json)
+    end
+  end
+
+  test 'should not allow repeated promotions' do
+    payload = {
+      promotion_id: 1,
+      organization_id: 3
+    }
+    assert_no_difference('UsageReport.count') do
+      @worker.work(payload.to_json)
+    end
+  end
+
+  test "should not record promotions with out promo's or org's id" do
+    payload = {
+      promotion_id: 1
+    }
+    assert_no_difference('UsageReport.count') do
+      @worker.work(payload.to_json)
+    end
+    payload = {
+      organization_id: 3
+    }
+    assert_no_difference('UsageReport.count') do
+      @worker.work(payload.to_json)
+    end
+  end
+
+  
 end

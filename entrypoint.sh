15b1807fe77025c6b76742257507ae88c9bdc44a
= <diegomarcel27@hotmail.com>
2019-11-12 14:21:18 -0300

    Al script de entrypoint se le agregan comandos de migraciones de base de datos, e inicializacion de los workers como background processes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /application/tmp/pids/server.pid
 
+rails RAILS_ENV=production db:create db:migrate db:seed
+rake rabbitmq:setup --trace
+
+WORKERS=CreatedPromotionsWorker,EvaluatedPromotionsWorker rake sneakers:run & disown
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"

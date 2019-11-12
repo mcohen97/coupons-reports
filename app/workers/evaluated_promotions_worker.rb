df137f02661c840bba561b3683ef7b66b5d9c4ee
= <diegomarcel27@hotmail.com>
2019-11-12 14:18:32 -0300

    Se agregan parametros de configuracion a Sneakers y sus workers

--- app/workers/evaluated_promotions_worker.rb
+++ app/workers/evaluated_promotions_worker.rb
@@ -1,7 +1,9 @@
 class EvaluatedPromotionsWorker
   include Sneakers::Worker
   # env is set to nil since by default the actual queue name would have the env appended to the end
-  from_queue ENV['EVALUATED_PROMOTIONS_QUEUE'], env: nil
+  from_queue ENV['EVALUATED_PROMOTIONS_QUEUE'],
+                env: nil,
+                durable: true
 
   # work method receives message payload in raw format
   # in our case it is JSON encoded string
--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,2 +1,6 @@
-Sneakers.configure({})
+Sneakers.configure :amqp => "amqp://guest:guest@#{ENV['EXCHANGE_SERVER_HOST']}:#{ENV['EXCHANGE_SERVER_PORT']}",
+                   :exchange => ENV['EXCHANGE_TOPIC'],
+                   :exchange_type => :topic,
+                   :durable => false
+
 Sneakers.logger.level = Logger::INFO # the default DEBUG is too noisy

895d961b9fb48f5b1d49888ef10d52e140024665
= <diegomarcel27@hotmail.com>
2019-11-12 23:53:27 -0300

    Se agrega campo al dto, que fue olvidado

--- app/dtos/usage_report_dto.rb
+++ app/dtos/usage_report_dto.rb
@@ -1,11 +1,12 @@
 class UsageReportDto
-  attr_reader :invocations_count, :negative_response_ratio, :positive_response_ratio, :average_response_time, :last_time_updated
+  attr_reader :invocations_count, :negative_response_ratio, :positive_response_ratio, :average_response_time, :last_time_updated, :total_money_spent
 
   def initialize(data)
     @invocations_count = data[:invocations_count]
     @negative_response_ratio = data[:negative_response_ratio]
     @positive_response_ratio = data[:positive_response_ratio]
     @average_response_time = data[:average_response_time]
+    @total_money_spent = data[:total_money_spent]
     @last_time_updated = data[:last_time_updated]
   end
 

201dad240e671409bba002f77b953ab2aa3f7ac9
= <diegomarcel27@hotmail.com>
2019-11-18 22:55:03 -0300

    Se actualizan las pruebas a los controllers que comenzaron a fallar cuando se actualizaron las rutas

--- test/controllers/demographic_reports_controller_test.rb
+++ test/controllers/demographic_reports_controller_test.rb
@@ -9,7 +9,7 @@ class DemographicReportsControllerTest < ActionDispatch::IntegrationTest
   APP_KEY2 = 'eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoicGVkaWRvc1lhS2V5Iiwib3JnYW5pemF0aW9uX2lkIjoxLCJwZXJtaXNzaW9ucyI6W119.PcH4QeiQMmRggz98s9nasVuwd4B30Y2m3WBhV_n0Xgc'
 
   test 'should generate report correctly ' do
-    get demographic_reports_url(1), headers: { Authorization: APP_KEY1 }
+    get api_v1_demographic_reports_url(1), headers: { Authorization: APP_KEY1 }
     assert_response :success
     report = JSON.parse(response.body)
     puts report.inspect
@@ -30,28 +30,28 @@ class DemographicReportsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should handle non existing reports' do
-    get demographic_reports_url(5), headers: { Authorization: APP_KEY1 }
+    get api_v1_demographic_reports_url(5), headers: { Authorization: APP_KEY1 }
     assert_response :not_found
     assert JSON.parse(response.body)['error_message']
   end
 
   test 'should not get report without app key' do
     # Report 2 is from organization 2, appkey is from organization 1
-    get demographic_reports_url(2)
+    get api_v1_demographic_reports_url(2)
     assert_response :unauthorized
     assert response.body['error_message']    
   end
 
   test "shouldn't not allow access to other organizations promotions" do
     # Report 2 is from organization 2, appkey is from organization 1
-    get demographic_reports_url(2), headers: { Authorization: APP_KEY1 }
+    get api_v1_demographic_reports_url(2), headers: { Authorization: APP_KEY1 }
     assert_response :forbidden
     assert response.body['error_message']
   end
 
   test "should not allow access to same organization's report without permission" do
     # APPKEY2 has no permission to get usage reports
-    get demographic_reports_url(1), headers: { Authorization: APP_KEY2 }
+    get api_v1_demographic_reports_url(1), headers: { Authorization: APP_KEY2 }
     assert_response :forbidden
     assert response.body['error_message']  
   end
--- test/controllers/usage_reports_controller_test.rb
+++ test/controllers/usage_reports_controller_test.rb
@@ -9,13 +9,13 @@ class UsageReportsControllerTest < ActionDispatch::IntegrationTest
   APP_KEY2 = 'eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoicGVkaWRvc1lhS2V5Iiwib3JnYW5pemF0aW9uX2lkIjoxLCJwZXJtaXNzaW9ucyI6WyJHRVRfREVNT0dSQVBISUNfUkVQT1JUIl19.1lhSou8nyMUpd5z2Z-RWDJRv78XgfVgFBRaP5NXw3Ho'
 
   test 'should handle non existing reports' do
-    get usage_reports_url(5), headers: { Authorization: APP_KEY1 }
+    get api_v1_usage_reports_url(5), headers: { Authorization: APP_KEY1 }
     assert_response :not_found
     assert JSON.parse(response.body)['error_message']
   end
 
   test 'should generate report correctly ' do
-    get usage_reports_url(1), headers: { Authorization: APP_KEY1 }
+    get api_v1_usage_reports_url(1), headers: { Authorization: APP_KEY1 }
     assert_response :success
     report = JSON.parse(response.body)
     assert_equal 2, report['invocations_count']
@@ -28,21 +28,21 @@ class UsageReportsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should not get report without app key' do
     # Report 2 is from organization 2, appkey is from organization 1
-    get usage_reports_url(2)
+    get api_v1_usage_reports_url(2)
     assert_response :unauthorized
     assert JSON.parse(response.body)['error_message']
   end
 
   test "shouldn't not allow access to other organizations promotions" do
     # Report 2 is from organization 2, appkey is from organization 1
-    get usage_reports_url(2), headers: { Authorization: APP_KEY1 }
+    get api_v1_usage_reports_url(2), headers: { Authorization: APP_KEY1 }
     assert_response :forbidden
     assert JSON.parse(response.body)['error_message']
   end
 
   test "should not allow access to same organization's report without permission" do
     # APPKEY2 has no permission to get usage reports
-    get usage_reports_url(1), headers: { Authorization: APP_KEY2 }
+    get api_v1_usage_reports_url(1), headers: { Authorization: APP_KEY2 }
     assert_response :forbidden
     assert JSON.parse(response.body)['error_message']
   end
